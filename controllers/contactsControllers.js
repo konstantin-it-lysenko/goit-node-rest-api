b866b85e4661962a35ef2b6ceba7191ee1b12b30
Konstantin Lysenko <konstantin.it.lysenko@gmail.com>
2024-02-20 02:30:32 +0200

    add CRUD controllers

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -1,11 +1,49 @@
-import contactsService from "../services/contactsServices.js";
+import { listContacts, getContactById, addContact, removeContact, updateContactById } from "../services/contactsServices.js";
+import asyncTryCatch from "../helpers/asyncTryCatch.js";
+import HttpError from "../helpers/HttpError.js";
 
-export const getAllContacts = (req, res) => {};
+export const getAllContacts = asyncTryCatch(async (_, res) => {
+    const contactList = await listContacts();
+    res.status(200).json(contactList);
+});
 
-export const getOneContact = (req, res) => {};
+export const getOneContact = asyncTryCatch(async (req, res) => {
+    const { id } = req.params;
+    const targetContact = await getContactById(id);
 
-export const deleteContact = (req, res) => {};
+    if (!targetContact) {
+        throw HttpError(404, "Not Found");
+    }
 
-export const createContact = (req, res) => {};
+    res.status(200).json(targetContact);
+});
 
-export const updateContact = (req, res) => {};
+export const createContact = asyncTryCatch(async (req, res) => {
+    const newContact = await addContact(req.body);
+    res.status(201).json(newContact);
+});
+
+export const updateContact = asyncTryCatch(async (req, res) => {
+    const { id } = req.params;
+    const targetContact = await updateContactById(id, req.body);
+
+    if (!targetContact) {
+        throw HttpError(404, "Not Found");
+    }
+
+    res.status(200).json(targetContact);
+});
+
+export const deleteContact = asyncTryCatch(async (req, res) => {
+    const { id } = req.params;
+    const targetContact = await removeContact(id);
+
+    if (!targetContact) {
+        throw HttpError(404, "Not Found");
+    }
+
+    res.json({
+        message: "Delete success"
+    })
+});
+
