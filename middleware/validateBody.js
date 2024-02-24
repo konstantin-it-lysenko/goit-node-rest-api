f912293611bcc1d999b23481c47a1010f22917f9
Konstantin Lysenko <konstantin.it.lysenko@gmail.com>
2024-02-24 20:28:48 +0200

    update and change import path

--- middleware/validateBody.js
+++ middleware/validateBody.js
@@ -1,19 +1,11 @@
-import HttpError from "../helpers/HttpError.js";
+import { HttpError } from "../helpers/index.js";
 
 const validateBody = (schema) => {
     const func = (req, _, next) => {
-        const fieldsQty = Object.keys(req.body).length;
-
         const { error } = schema.validate(req.body);
-
-        if (!fieldsQty) {
-            next(HttpError(400, "Body must have at least one field"));
-        }
-
         if (error) {
             next(HttpError(400, error.message));
         }
-
         next();
     };
 
