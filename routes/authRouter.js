ae8a86ec5be5ae8a71ec7940779d6b7f642c1844
Konstantin Lysenko <konstantin.it.lysenko@gmail.com>
2024-02-25 02:07:37 +0200

    add patch auth router for upload avatar

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -2,6 +2,7 @@ import express from "express";
 import { validateBody, authenticate } from "../middleware/index.js";
 import { registerSchema, loginSchema, updateSubscriptionSchema } from "../models/index.js";
 import { register, login, getCurrent, logout, updateSubscription } from "../controllers/authContollers.js";
+import upload from "../middleware/upload.js";
 
 const authRouter = express.Router()
 
@@ -18,6 +19,13 @@ authRouter.patch(
     authenticate,
     validateBody(updateSubscriptionSchema),
     updateSubscription
+);
+
+authRouter.patch(
+    "/",
+    authenticate,
+    upload.single("avatar"),
+    uploadAvatar
 );
 
 export default authRouter;
