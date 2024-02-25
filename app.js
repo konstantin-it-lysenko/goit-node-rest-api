14958994e1a1b710ca3a94e69bab11d2abb3661b
Konstantin Lysenko <konstantin.it.lysenko@gmail.com>
2024-02-25 17:04:10 +0200

    update auth route

--- app.js
+++ app.js
@@ -12,8 +12,9 @@ export const app = express();
 app.use(morgan("tiny"));
 app.use(cors());
 app.use(express.json());
+app.use(express.static("public"));
 
-app.use("/api/auth", authRouter);
+app.use("/users", authRouter);
 app.use("/api/contacts", contactsRouter);
 
 app.use((_, res) => {
